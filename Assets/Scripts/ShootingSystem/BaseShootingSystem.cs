07e964f9b8af473112f9b0b65c838aa655522ce7
Ushien <antoinevanaelst@hotmail.com>
2025-07-25 16:52:44 +0200

    On peut envoyer des fusées à volonté
    3 2 1 feu

--- Assets/Scripts/ShootingSystem/BaseShootingSystem.cs
+++ Assets/Scripts/ShootingSystem/BaseShootingSystem.cs
@@ -8,33 +8,14 @@ namespace ShootingSystem {
         [SerializeField] private Transform projectileSpawnPoint; /// Spawn point for the projectiles.
         [SerializeField] private AudioClip shotSound; /// Sound played when a projectile is shot.
         [SerializeField] private AudioSource audioSource; /// Audio source to play the shot sound.
-        [SerializeField] private float projectileSpeed = 20f; /// The speed of the projectile.
-        [SerializeField] private float distance = 0f; /// The distance the projectile will travel before returning to the pool.
-        [SerializeField] private Vector2 projectileSpreadVariance = new Vector2(0f, 0f); /// Variance in the projectile's direction to create a spread effect.
 
         /// <summary>
         /// Shoots a projectile in the specified direction with a spread effect.
         /// </summary>
         public void Shoot()
         {
-            Vector2 direction = GetDirection();
             Vector2 startPosition = projectileSpawnPoint.position;
-            Vector2 targetPosition = startPosition + direction * distance;
-            ShotEffects(startPosition, targetPosition);
-        }
-
-        /// <summary>
-        /// Gets a upward direction, applying a spread effect.
-        /// </summary>
-        protected Vector2 GetDirection()
-        {
-            Vector2 direction = transform.up;
-            direction += new Vector2(
-                Random.Range(-projectileSpreadVariance.x, projectileSpreadVariance.x),
-                Random.Range(-projectileSpreadVariance.y, projectileSpreadVariance.y)
-            );
-            direction.Normalize();
-            return direction;
+            ShotEffects(startPosition);
         }
 
         /// <summary>
@@ -51,7 +32,7 @@ namespace ShootingSystem {
         /// <summary>
         /// Handles the effects of shooting a projectile, including instantiation and initialization.
         /// </summary>
-        protected void ShotEffects(Vector2 startPosition, Vector2 targetPosition)
+        protected void ShotEffects(Vector2 startPosition)
         {
             GameObject projectile = projectilePool.GetObject();
             projectile.transform.position = projectileSpawnPoint.position;
@@ -59,7 +40,7 @@ namespace ShootingSystem {
             FireworkProjectile projectileBehaviour = projectile.GetComponent<FireworkProjectile>();
             if (projectileBehaviour != null)
             {
-                projectileBehaviour.Init(projectileSpeed, distance, projectilePool, startPosition, targetPosition);
+                projectileBehaviour.Init(projectilePool, startPosition);
                 PlayShotSound();
             }
         }    
--- Assets/Scripts/ShootingSystem/FireworkProjectile.cs
+++ Assets/Scripts/ShootingSystem/FireworkProjectile.cs
@@ -6,51 +6,17 @@ namespace ShootingSystem {
     public class FireworkProjectile : MonoBehaviour
     {
         private ObjectPool pool; /// The object pool managing the projectile instances.
-        private float projectileSpeed; /// The speed of the projectile.
-        private float distance; /// The distance the projectile will travel before returning to the pool.
-        private float remainingDistance; /// The remaining distance the projectile will travel before returning to the pool.
         private Vector2 startPosition; /// The starting position of the projectile.
-        private Vector2 targetPosition; /// The target position the projectile will move towards.
 
         /// <summary>
         /// Initializes the projectile with the specified parameters.
         /// </summary>
-        /// <param name="projectileSpeed">The speed of the projectile (unused in this implementation).</param>
         /// <param name="pool">The object pool managing the projectile instances.</param>
-        /// <param name="distance">The distance the projectile will travel before returning to the pool.</param>
         /// <param name="startPosition">The starting position of the projectile.</param>
-        /// <param name="targetPosition">The target position the projectile will move towards.</param>
-        public void Init(float projectileSpeed, float distance, ObjectPool pool, Vector2 startPosition, Vector2 targetPosition)
+        public void Init(ObjectPool pool, Vector2 startPosition)
         {
             this.startPosition = startPosition;
-            this.targetPosition = targetPosition;
-            this.projectileSpeed = projectileSpeed;
             this.pool = pool;
-            this.distance = distance;
-            this.remainingDistance = distance;
-        }
-
-        /// <summary>
-        /// Moves the projectile towards the target position and handles the impact effect.
-        /// </summary>
-        private void Move()
-        {
-            if(remainingDistance > 0){
-                transform.position = Vector2.Lerp(startPosition, targetPosition, 1 - (remainingDistance / distance));
-                remainingDistance -=  projectileSpeed * Time.deltaTime;
-            }
-            else{
-                //Impact effect can be added here
-                pool.ReturnObject(gameObject);
-            }
-        }
-
-        /// <summary>
-        /// Updates the projectile's position each frame.
-        /// </summary>
-        private void Update()
-        {
-            Move();
         }
     }
 }
