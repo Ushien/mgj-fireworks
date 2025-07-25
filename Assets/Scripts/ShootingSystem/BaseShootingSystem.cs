82a454a09fd999d13c62a062da0b735fa2862c83
Gillet Antoine <mandrass75@gmail.com>
2025-07-25 14:38:07 +0200

    Added doc to Base Shooting

--- Assets/Scripts/ShootingSystem/BaseShootingSystem.cs
+++ Assets/Scripts/ShootingSystem/BaseShootingSystem.cs
@@ -4,14 +4,17 @@ using Utils;
 namespace ShootingSystem {
     public class BaseShootingSystem : MonoBehaviour
     {
-        [SerializeField] private ObjectPool projectilePool;
-        [SerializeField] private Transform projectileSpawnPoint;
-        [SerializeField] private AudioClip shotSound;
-        [SerializeField] private AudioSource audioSource;
-        [SerializeField] private float projectileSpeed = 20f;
-        [SerializeField] private float distance = 0f;
-        [SerializeField] private Vector2 projectileSpreadVariance = new Vector2(0f, 0f);
+        [SerializeField] private ObjectPool projectilePool; /// The object pool managing the projectile instances.
+        [SerializeField] private Transform projectileSpawnPoint; /// Spawn point for the projectiles.
+        [SerializeField] private AudioClip shotSound; /// Sound played when a projectile is shot.
+        [SerializeField] private AudioSource audioSource; /// Audio source to play the shot sound.
+        [SerializeField] private float projectileSpeed = 20f; /// The speed of the projectile.
+        [SerializeField] private float distance = 0f; /// The distance the projectile will travel before returning to the pool.
+        [SerializeField] private Vector2 projectileSpreadVariance = new Vector2(0f, 0f); /// Variance in the projectile's direction to create a spread effect.
 
+        /// <summary>
+        /// Shoots a projectile in the specified direction with a spread effect.
+        /// </summary>
         public void Shoot()
         {
             Vector2 direction = GetDirection();
@@ -19,12 +22,10 @@ namespace ShootingSystem {
             Vector2 targetPosition = startPosition + direction * distance;
             ShotEffects(startPosition, targetPosition);
         }
-        
-        public virtual void Canceled()
-        {
-            throw new System.NotImplementedException();
-        }
 
+        /// <summary>
+        /// Gets a upward direction, applying a spread effect.
+        /// </summary>
         protected Vector2 GetDirection()
         {
             Vector2 direction = transform.up;
@@ -36,14 +37,21 @@ namespace ShootingSystem {
             return direction;
         }
 
-        protected void PlayShotSound(){
+        /// <summary>
+        /// Plays the shot sound if available.
+        /// </summary>
+        protected void PlayShotSound()
+        {
             if (shotSound != null && audioSource != null)
             {
                 audioSource.PlayOneShot(shotSound);
             }
         }
 
-        protected void ShotEffects(Vector2 startPosition, Vector2 targetPosition) 
+        /// <summary>
+        /// Handles the effects of shooting a projectile, including instantiation and initialization.
+        /// </summary>
+        protected void ShotEffects(Vector2 startPosition, Vector2 targetPosition)
         {
             GameObject projectile = projectilePool.GetObject();
             projectile.transform.position = projectileSpawnPoint.position;
@@ -51,7 +59,7 @@ namespace ShootingSystem {
             FireworkProjectile projectileBehaviour = projectile.GetComponent<FireworkProjectile>();
             if (projectileBehaviour != null)
             {
-                projectileBehaviour.Init(projectileSpeed,distance,projectilePool,startPosition,targetPosition);
+                projectileBehaviour.Init(projectileSpeed, distance, projectilePool, startPosition, targetPosition);
                 PlayShotSound();
             }
         }    
