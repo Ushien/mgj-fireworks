9c0875651a9131526013c6a540c4487203d81647
Thomas <Thgi2706@gmail.com>
2025-07-27 18:16:09 +0200

    yellow powder update

--- Assets/Scripts/PowdersModificators/YellowPowder.cs
+++ Assets/Scripts/PowdersModificators/YellowPowder.cs
@@ -1,13 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class YellowPowder : PowderModificator
 {
     // il faut mettre la poudre dans la scene et ensuite rajoute la texture et le sprite renderer.
     [SerializeField]
-    private SpriteRenderer spriteRenderer;
+    List<SpriteRenderer> spritesRenderers;
     
     [SerializeField]
-    private Texture2D texture;
+    List<Texture2D> textures;
     override public void ApplyModifier()
     {
         if (attachedFirework.name == "Fireworks(Clone)")
@@ -15,12 +16,20 @@ public class YellowPowder : PowderModificator
             // permet de faire une explosion dans la forme du spriterender/texture
             ParticleSystem.SubEmittersModule subEmitters = attachedFirework.subEmitters;
             ParticleSystem burstSystem = subEmitters.GetSubEmitterSystem(1);
+            ParticleSystem.EmissionModule emission = burstSystem.emission;
+            ParticleSystem.Burst[] bursts = new ParticleSystem.Burst[emission.burstCount];
+            emission.GetBursts(bursts);
+            float numEmitted = bursts[0].count.constant;
+            numEmitted = 750;
+            bursts[0].count = new ParticleSystem.MinMaxCurve(numEmitted);
+            emission.SetBursts(bursts);
             ParticleSystem.ShapeModule shapeModule = burstSystem.shape;
             shapeModule.shapeType = ParticleSystemShapeType.SpriteRenderer;
             shapeModule.meshShapeType = ParticleSystemMeshShapeType.Triangle;
-            shapeModule.spriteRenderer = spriteRenderer;
-            shapeModule.texture = texture;
-            shapeModule.scale = new Vector3(20,20,20);
+            int randomIndex = Random.Range(0, textures.Count); 
+            shapeModule.spriteRenderer = spritesRenderers[randomIndex];
+            shapeModule.texture = textures[randomIndex];
+            shapeModule.scale = new Vector3(10,10,10);
         }
     }
 }
