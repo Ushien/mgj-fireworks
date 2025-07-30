bc2090afb140d7e1763037d56074eaa19a1cb73d
alexandreperrot <39654281+alexandreperrot@users.noreply.github.com>
2025-07-30 20:53:27 +0200

    brown powder 1

--- Assets/Scripts/PowdersModificators/BrownPowder.cs
+++ Assets/Scripts/PowdersModificators/BrownPowder.cs
@@ -5,14 +5,22 @@ public class BrownPowder : PowderModificator
     // Augmente le nombre de particules de l'explosion principale
     override public void ApplyModifier()
     {
-        // Si on manipule l'explosion principale
         var subEmitters = attachedFirework.subEmitters;
+
         if (attachedFirework.name == "Fireworks(Clone)")
         {
             ParticleSystem burstSystem = subEmitters.GetSubEmitterSystem(1);
             ParticleSystem trailSystem = burstSystem.subEmitters.GetSubEmitterSystem(0);
-            ParticleSystem.MainModule mm = trailSystem.main;
-            mm.startLifetime = new ParticleSystem.MinMaxCurve(2f,3f); // Augmente la durée de vie des particules du trail
+
+            // Modify main module
+            var main = trailSystem.main;
+            main.startLifetime = new ParticleSystem.MinMaxCurve(0.1f, 10f);
+            main.maxParticles = 10000;
+
+            // Modify emission module
+            var emission = trailSystem.emission;
+            emission.rateOverTime = new ParticleSystem.MinMaxCurve(0f);     // example value
+            emission.rateOverDistance = new ParticleSystem.MinMaxCurve(5f);  // example value
         }
     }
 }
